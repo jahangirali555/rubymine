ef9bb0cea898aed470d2349eee079306a331f244
jahangirali555 <jahangirali555@gmail.com>
2014-06-18 17:32:34 +0500

    Testing Email rails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,12 @@ class UserMailer < ActionMailer::Base
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+
+  def send_signup_email(user)
+    @user = user      
+      mail( to: 'jahangirali555@gmail.com',
+    subject: 'Thanks for signing up for our amazing app' )
+  
+   
+    end
 end
