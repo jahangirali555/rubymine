c1d28f4ee4d417a2669a9d8be5454e61774d2a7c
jahangirali555 <jahangirali555@gmail.com>
2014-06-18 17:39:48 +0500

    Testing Email railss

--- config/environment.rb
+++ config/environment.rb
@@ -3,18 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-
-
-# #Configurations for sending emails using rails
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#   :user_name => 'jahangirali555@gmail.com',
-#   :password => '@alikhan999',
-#   :domain => 'gmail.com',
-#   :address => 'smtp.gmail.com',
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-}
-
